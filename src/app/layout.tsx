d86c80d4459a17704d08240fad9c85e848992da0
dicethedev <dicethedev@gmail.com>
2025-05-08 16:03:46 +0100

    impr: add a better feature to providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,12 @@
+import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
-import { Providers } from "@/constants/providers";
+import { headers } from "next/headers";
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { cookieToInitialState } from "wagmi";
+import { Providers } from "./providers";
+import getConfig from "next/config";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,18 +24,23 @@ export const metadata: Metadata = {
     "BaseSwap - Seamless token transfers and swaps without approval popups, powered by Base Smart Wallet Sub Accounts",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const headersList = await headers();
+  const initialState = cookieToInitialState(
+    getConfig(),
+    headersList.get("cookie")
+  );
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>{children}</Providers>
         <Toaster position="top-right" />
+        <Providers initialState={initialState}>{children}</Providers>
       </body>
     </html>
   );
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,11 +3,14 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { type ReactNode, useState } from "react";
 import { type State, WagmiProvider } from "wagmi";
-import { config } from "@/lib/wagmi";
+
+import { getConfig } from "@/lib/wagmi";
+
 export function Providers(props: {
   children: ReactNode;
   initialState?: State;
 }) {
+  const [config] = useState(() => getConfig());
   const [queryClient] = useState(() => new QueryClient());
 
   return (
